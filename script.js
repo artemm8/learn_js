96738ffc7e5845c006a32fabc50ffd9ed8e68ca9
Artem <artem907122@gmail.com>
2023-01-14 12:55:51 -0500

    2.0

---
+++ script.js
@@ -0,0 +1,74 @@
+// document.getElementById("testdema").innerHTML="newtext"
+// // измениние содержимомо в теге
+// document.write(1+1)
+// window.alert(321+123)
+// console.log(543+123)
+
+// // var a,b,c 
+// // a=126
+// // b=556
+// // c=a+b
+// // console.log(c)
+//  // $a=10
+// // _b=4
+// // // console.log($a)
+
+// // let y=3 
+// // // let y=5
+// // // второй раз let нельзя создавать
+
+// // {
+// //     let o=9 
+// // }
+// // // console.log(o) нельзя использавать let не в блок
+// // const k=88
+// // // k=10 const переменные нельзя изменять
+// // var car=["audi","bmw","honda"]
+// // console.log(car)
+// // // массив
+// // var food={soup:1.99,pizza:4.99}
+// // console.log(food)
+// // // обьект
+
+
+
+// function test(a,b,c){
+//     return a+b+c
+// }
+
+// var x=test(1,2,3)
+// console.log(x)
+// var people={
+
+//     first_name:"alexandr",
+//     last_name:"toporov",
+//     age:"13",
+
+//     full_name:function full_name(){
+//         return this.first_name+ " "+this.last_name
+//     }
+// }
+// console.log(people.full_name())
+
+
+// var x="test2"
+// console.log(x.length)
+// console.log(10+"10")
+
+
+// var de = Date()
+// console.log(de.getFullYear())
+// console.log(Math.random())
+// console.log(1<2)
+
+
+
+x=12
+if (x>12){
+
+    console.log("x больше 12")
+}else if (x<12){
+    console.log("x меньше двиналцыии")
+}else {
+    console.log("x=12")
+}
